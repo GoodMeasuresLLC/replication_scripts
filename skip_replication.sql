ebfd2f501e28bca2eda637ea0c2a1a13ee09b44c
rob mathews <rob.mathews@goodmeasures.com>
2023-12-23 16:20:08 -0500

    extra note on skipping replication.

--- skip_replication.sql
+++ skip_replication.sql
@@ -1,2 +1,11 @@
  -- get the lsn from the /var/log/syslog error message
  ALTER SUBSCRIPTION public SKIP (lsn = 'F4/DAABEED0');
+
+
+-- you can find the latest LSN on the reporting db (the subscriber) by doing
+select pg_current_wal_lsn();
+-- which gives: 6FC/93BA5F80
+-- and then you can skip past 6FC/93BA5F80 by doing
+-- 6FC/93BA5F80+1 = 6FC/93BA5F81
+-- and on the reporting db (the subscriber)
+ALTER SUBSCRIPTION public SKIP (lsn = '6FC/93BA5F81');
